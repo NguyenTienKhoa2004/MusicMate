57d82fb61eefdd489303f9a82e3c38c4dd0d8068
Nguyen Tien Khoa <ducrua1311@gmail.com>
2025-11-27 20:10:43 +0700

    update

--- MusicMate.Domain/Entities/MusicGenre.cs
+++ MusicMate.Domain/Entities/MusicGenre.cs
@@ -3,8 +3,6 @@
 public class MusicGenre
 {
     public int Id { get; set; }
-    public string Name { get; set; } = null!; // V-Pop, K-Pop, US-UK, Ballad, EDM...
-    public string? SpotifyGenreId { get; set; } // để sau sync với Spotify API
-
+    public string Name { get; set; } = null!; 
     public ICollection<UserFavoriteGenre> Users { get; set; } = new List<UserFavoriteGenre>();
 }
